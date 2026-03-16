e4f11cba617a3676ad05231fe842b5fc2daf587d
boxinthechaos <kdmwp310@gmail.com>
2026-03-16 18:17:07 +0900

    add emailService

--- src/main/java/org/example/gam/controller/AuthController.java
+++ src/main/java/org/example/gam/controller/AuthController.java
@@ -1,10 +1,9 @@
 package org.example.gam.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.gam.dto.JoinRequest;
-import org.example.gam.dto.LoginRequest;
-import org.example.gam.dto.TokenResponse;
+import org.example.gam.dto.*;
 import org.example.gam.service.AuthService;
+import org.example.gam.service.EmailService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AuthController {
     private final AuthService authService;
+    private final EmailService emailService;
 
     @PostMapping("/join")
     public ResponseEntity<String> join(@RequestBody JoinRequest request){
@@ -24,5 +24,22 @@ public class AuthController {
     public ResponseEntity<TokenResponse> login(@RequestBody LoginRequest loginRequest){
         TokenResponse tokenResponse = authService.login(loginRequest);
         return ResponseEntity.ok(tokenResponse);
+    }
+
+    @PostMapping("/email/send")
+    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest request){
+        emailService.sendVerificationCode(request.getEmail());
+        return ResponseEntity.ok("이메일 인증코드가 성공적으로 발송되었습니다. 5분 이내로 확인해주세요");
+    }
+
+    @PostMapping("/email/verify")
+    public ResponseEntity<String> verifyEmail(@RequestBody EmailVerifyRequest request) {
+        boolean isVerified = emailService.verifyCode(request.getEmail(), request.getCode());
+
+        if (isVerified) {
+            return ResponseEntity.ok("이메일 인증이 완료되었습니다.");
+        } else {
+            return ResponseEntity.badRequest().body("인증 번호가 틀렸거나 만료되었습니다.");
+        }
     }
 }
---
+++ src/main/java/org/example/gam/service/EmailService.java
@@ -0,0 +1,44 @@
+package org.example.gam.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+    private final StringRedisTemplate redisTemplate;
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+    public void sendVerificationCode(String toEmail){
+        String code = String.valueOf((int) (Math.random() * 899999) + 100000);
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(toEmail);
+        message.setFrom(senderEmail);
+        message.setSubject("[Gam] 여행 앱 회원가입 이메일 인증번호 입니다.");
+        message.setText("인증번호는 [" + code +"] 입니다. 5분 이네에 입력해 주세요");
+        javaMailSender.send(message);
+
+        redisTemplate.opsForValue().set(toEmail, code, Duration.ofMinutes(5));
+    }
+
+    public boolean verifyCode(String email, String inputCode){
+        String saveCode = redisTemplate.opsForValue().get(email);
+
+        if(saveCode != null && saveCode.equals(inputCode)){
+            redisTemplate.delete(email);
+            return true;
+        }
+        return false;
+    }
+}
